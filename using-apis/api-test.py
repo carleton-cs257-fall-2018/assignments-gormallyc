4d76f2dfe677fe2122180df6b7f7a20fa7519095
Conor Gormally <ConorGormally@eduroam-163-19.dyn.carleton.edu>
2018-10-01 23:56:46 -0500

    last changes for tonight, API queries still being rejected after one or even no runs of the script. Fuck everything

--- using-apis/api-test.py
+++ using-apis/api-test.py
@@ -1,18 +1,20 @@
 '''
-    api-test.py
     Conor Gormally
+    9/30/18
+    A program that uses the Google Places api to get information about places based on user input
 '''
 
 import sys, argparse, json, urllib.request
 
 def get_list_of_places(query):
     '''
-
+    Gets a list of places based on user-inputted search text
     '''
     query =query.replace(' ', '+')
-    base_url = 'https://maps.googleapis.com/maps/api/place/textsearch/json?query={0}&key=AIzaSyDC7uCch4XQ8yYVizaXDJHKspfwIFpKqHw'
+    base_url = 'https://maps.googleapis.com/maps/api/place/textsearch/json?query={0}&key=AIzaSyC8juvEv-Mws-Ei7RWCGD_ABLXLsmy2Su4'
     url = base_url.format(query)
     print(url)
+    #gets and decodes json data from the API
     data_from_server = urllib.request.urlopen(url).read()
     string_from_server = data_from_server.decode('utf-8')
     places_from_key = json.loads(string_from_server)
@@ -22,42 +24,31 @@ def get_list_of_places(query):
         type_of_place = place['type']
         location_of_place = place['formatted_address']
         place_id = place['place_id']
-        if type(name_of_place) != type(''):
-            raise Exception('name has wrong type: "{0}"'.format(name_of_place))
-        if type(location_of_place) != type(''):
-            raise Exception('location of place has wrong type: "{0}"'.format(location_of_place))
         result_list.append({'name':name_of_place, 'location':location_of_place})
     return result_list
 
 def get_more_information(name_query):
     '''
-
+    Gets information about a specific place
     '''
     name_query = name_query.replace(' ', '%20')
-    base_url = 'https://maps.googleapis.com/maps/api/place/findplacefromtext/json?&input={0}&inputtype=textquery&fields=name,formatted_address,rating,opening_hours,types&key=AIzaSyDC7uCch4XQ8yYVizaXDJHKspfwIFpKqHw'
+    base_url = 'https://maps.googleapis.com/maps/api/place/findplacefromtext/json?&input={0}&inputtype=textquery&fields=name,formatted_address,rating,opening_hours,types&key=AIzaSyC8juvEv-Mws-Ei7RWCGD_ABLXLsmy2Su4'
     url = base_url.format(name_query)
     print(url)
+    #gets and decodes json data from the API 
     data_from_server = urllib.request.urlopen(url).read()
     string_from_server = data_from_server.decode('utf-8')
     place_information = json.loads(string_from_server)
+    print(place_information)
     return_information_list = []
-    for info_chunk in place_information:
+    for info_chunk in place_information['candidates']:
+        print(info_chunk)
+        print(info_chunk.keys())
         place_name = info_chunk['name']
         place_location = info_chunk['formatted_address']
         place_rating = info_chunk['rating']
-        place_hours = info_chunk['opening_hours']
         place_types = info_chunk['types']
-        if type(place_name) != type(''):
-            raise Exception('name has wrong type: "{0}"'.format(place_name))
-        if type(place_location) != type(''):
-            raise Exception('location of place has wrong type: "{0}"'.format(place_location))
-        if type(place_rating) != type(''):
-            raise Exception('rating of place has wrong type: "{0}"'.format(place_rating))
-        if type(place_hours) != type(''):
-            raise Exception('hours of place has wrong type: "{0}"'.format(place_hours))
-        if type(place_types) != type([]):
-            raise Exception('type of places has wrong type: "{0}"'.format(place_types))
-        information_list.append({'name':place_name, 'location':place_location, 'rating':place_rating, 'hours':place_hours, 'types':place_types})
+        return_information_list.append({'name':place_name, 'location':place_location, 'rating':place_rating, 'types':place_types})
     return return_information_list
 
 def main(args):
@@ -74,15 +65,10 @@ def main(args):
             name = info_chunk['name']
             location = info_chunk['formatted_address']
             rating = info_chunk['rating']
-            hours = info_chunk['opening_hours']
             types = info_chunk['types']
-            print('{0} [{1} {2} {3} {4}]'.format(name, location, rating, hours, types))
+            print('{0} [{1} {2} {3} {4}]'.format(name, location, rating, types))
 
-if __name__ == '__main__':
-    # When I use argparse to parse my command line, I usually
-    # put the argparse setup here in the global code, and then
-    # call a function called main to do the actual work of
-    # the program.
+if _name_ == '_main_':
     parser = argparse.ArgumentParser(description='Get places and place information from Google Places API')
 
     parser.add_argument('querytext',
